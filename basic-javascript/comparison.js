4544f250426d707f243434b4a0d0ba1e94055cad
Behan kumar <83676505+behan05@users.noreply.github.com>
2023-12-27 20:06:02 +0000

    recap comparison of datas

---
+++ basic-javascript/comparison.js
@@ -0,0 +1,26 @@
+
+// let uname = "Behan";
+// console.log(uname[0]);
+
+// Before the learn about condition statement need to know about comparision..
+
+// So the Basic Comparison:
+
+// console.log(2 > 1);
+// console.log(1 < 2);
+// console.log(1 != 3);
+// console.log(4 !== "4");
+// console.log(2 >= 1);
+// console.log(1 <= 2);
+// console.log(2 === 1);
+// console.log(1 == 2); // And so on..
+
+let srt = "4";
+let num = 4;
+
+// Now try to compare.
+// console.log(srt == num); // output is 'true' why because javascript runtime environment first string "4" converted in number and than compare.
+// console.log(typeof srt, typeof num)
+
+// console.log(srt === num); // finally i get false why because when use "===" means now javascript runtime environment check strictly check there data type.
+
