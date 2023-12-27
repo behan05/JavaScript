2b7b9973216ad0127a135d7938ec42e1b250a424
Behan kumar <83676505+behan05@users.noreply.github.com>
2023-12-27 17:40:30 +0000

    work with some data type

---
+++ basic-javascript/type-conversion.js
@@ -0,0 +1,25 @@
+// try to understand the working of data type.
+
+// let userName = "behan";
+// let userAge = 24;
+// let isUserLoggedIn = true;
+// let userCity = null;
+// let userEmail;
+
+// let symbol, bigInt // not use friquently.
+
+// console.log(typeof userName);
+// console.log(userName);
+
+// console.log(typeof userAge);
+// console.log(userAge);
+
+// console.log(typeof isUserLoggedIn);
+// console.log(isUserLoggedIn);
+
+// console.log(typeof userCity);
+// console.log(userCity);
+
+// console.log(typeof userEmail);
+// console.log(userEmail);
+
