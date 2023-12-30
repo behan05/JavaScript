334c8dffd5b63d20a9ba0ac9f9ef7b5bc1af183d
Behan kumar <83676505+behan05@users.noreply.github.com>
2023-12-30 12:58:01 +0000

    Leearn that how javaScript use memory

---
+++ basic-javascript/memoryInJavaScript.js
@@ -0,0 +1,23 @@
+// there are two way to use Memory by javascript data type.
+
+// Stak Memory(Primitive), Heap Memory(Non-Primitive/Reference).
+
+
+let name = "behan";
+let anotherName = name;
+anotherName = "sitesh";
+
+console.log(name);
+console.log(anotherName);
+
+const user = {
+    userEmail:"behan@google.com",
+    userUpiId:"behan@okybl"
+}
+
+let userTwo = user;
+
+userTwo.userEmail = "sitesh@microshoft.com";
+
+console.log(user.userEmail);
+console.log(userTwo.userEmail);
