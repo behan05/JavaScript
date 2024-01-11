93750e4b471df4d6b93134e9308b3491f6727bef
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-01-11 18:02:34 +0000

    nothing to commit

---
+++ controlflow/truthly.js
@@ -0,0 +1,15 @@
+// falsly values
+
+// false , 0, -1, "", BigInt 0n, null, undefined
+
+// truthly values
+
+// true, 1, " ", [], {}, 
+
+const val = 0;
+if(val){
+    console.log("false value");
+}
+
+
+
