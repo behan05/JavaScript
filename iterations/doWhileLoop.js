fc7270402a1f88fb664bad0cec7cfbfc8858495c
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-01-12 12:41:55 +0000

    understand about while loop & do while loop and some use case of it

---
+++ iterations/doWhileLoop.js
@@ -0,0 +1,17 @@
+// Do while lopp
+
+let n = 10;
+let inst = 1
+
+// do {
+//     console.log(inst);
+//     inst++;
+// } while (inst <= n);
+
+const arr = [1,2,3,4,5,6,7];
+let index = 0;
+
+do {
+    console.log(arr[index]);
+    index++;
+} while (index < arr.length);
---
+++ iterations/whileLoop.js
@@ -0,0 +1,19 @@
+// while loop
+
+let num = 1;
+let i = 5;
+
+// while (num <= i) {
+//     console.log(num);
+//     num = num + 1;
+// }
+
+const myArray = [1,2,3,4,5,6,7];
+let index = 0
+
+// while (index < myArray.length) {
+//     const element = myArray[index];
+//     console.log(element);
+//     index++;
+// }
+
