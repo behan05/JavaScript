2c8c71886dd6ac754df2d804339949f5037e4f6f
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-02-07 05:26:57 +0000

    prototype

---
+++ 01_advance/prototype.js
@@ -0,0 +1,29 @@
+//as we know in javaScript everything comes from the object.
+
+
+// const obj = new Object;
+// console.log(typeof obj);
+
+// const person = {
+//     pname: "behan",
+//     pemail: "behankrbth@gmail.com"
+// }
+// const student = {
+//     uname: "sitesh",
+//     email: "siteshkrbth@gmail.com",
+//     thanks: function () { 
+//         console.log(`thanks ${pname}`);
+//     }
+// }
+
+// const str = "behan   "
+// console.log(str.length);
+
+// Object.prototype.trueLength = () => {
+//     console.log(`${this.str}`);
+// }
+
+// console.log(str.trueLength());
+
+// inheritence.
+person.__proto__(student);
