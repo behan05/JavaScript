a33aa1ab6835f0086c5264a3bbfa5cb068be6e8f
behan05 <behankrbth@gmail.com>
2024-03-05 07:05:08 +0530

    learn unary operator

--- 01_basic-javascript/increment-decrement.js
+++ 01_basic-javascript/increment-decrement.js
@@ -1,1 +1,39 @@
- 
+// somtime called as unary operator => 
+
+// 1 => pre-increment ++ , pre-decrement --
+// 2 => post-increment ++ , post-decrement --
+
+
+// 1 => pre-increment ++ , pre-decrement -- Example:
+
+// let x = 5;
+// let y = ++x;
+// console.log(x);
+// console.log(y);
+// let a = 5;
+// let b = --a;
+// console.log(a);
+// console.log(b);
+
+// 2 => post-increment ++ , post-decrement -- Example:
+
+// let x = 5;
+// let y = x++;
+// console.log(x);
+// console.log(y);
+
+// let a = 5;
+// let b = a--;
+// console.log(a);
+// console.log(b);
+
+// post increment
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//     // ++i;
+// }
+
+// post decrement
+// for (let i = 5; i > 0; i--) {
+//     console.log(i);
+// }
