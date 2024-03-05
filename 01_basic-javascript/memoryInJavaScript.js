0f77e6271586230e4955a27cc3ec550a13920401
behan05 <behankrbth@gmail.com>
2024-03-05 07:28:06 +0530

    understand stack and heap memory how work data type in there memory

--- 01_basic-javascript/memoryInJavaScript.js
+++ 01_basic-javascript/memoryInJavaScript.js
@@ -2,23 +2,55 @@
 
 // Stak Memory(Primitive), Heap Memory(Non-Primitive/Reference).
 
-let name = "behan";
-let anotherName = name;
-anotherName = "sitesh";
+// let name = "behan";
+// let anotherName = name;
+// anotherName = "sitesh";
 
-console.log(name);
-console.log(anotherName);
+// console.log(name);
+// console.log(anotherName);
+
+// const user = {
+//     userEmail:"behan@google.com",
+//     userUpiId:"behan@okybl"
+// }
+
+// let userTwo = user;
+
+// userTwo.userEmail = "sitesh@microshoft.com";
+
+// console.log(user.userEmail);
+// console.log(userTwo.userEmail);
+
+// second time practice:
+
+// there are two way to use memory by javascript:
+
+// 1: Stack(used by primitive data type), 2: Heap(used by none-primitive or Reference).
+
+// let userName = "behan";
+// let userEmail = "behan@google.com";
+
+// userEmail = "sitesh@yahoo.com";
+// console.log(userEmail);
+
+// let anotherUserName = userName;
+// anotherUserName = "sitesh";
+
+// console.log(anotherUserName);
+// console.log(userName);
 
 const user = {
     userEmail:"behan@google.com",
     userUpiId:"behan@okybl"
 }
 
-let userTwo = user;
+const userTwo = user;
 
-userTwo.userEmail = "sitesh@microshoft.com";
+userTwo.userEmail = "sitesh@gmail.com";
 
-console.log(user.userEmail);
 console.log(userTwo.userEmail);
+console.log(user.userEmail);
 
-   
+// console.log(user.userEmail);
+// user.userEmail = "sitesh@fackbook.com";
+// console.log(user.userEmail);
