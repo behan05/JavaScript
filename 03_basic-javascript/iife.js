485b6d9e4e80ef4ae83c4bdc1ecd2f58aedd801c
behan05 <behankrbth@outlook.com>
2024-03-07 07:02:42 +0530

    done some exercise with IIFE(Immediate Invoked Function Expression)

--- 03_basic-javascript/iife.js
+++ 03_basic-javascript/iife.js
@@ -2,19 +2,62 @@
 
 // Use of IIFE: that function which need to execute immediately.
 
-(function one(){
+(function one() {
     console.log(`DB Connected`);  // named Function
 })();
 
 
-(function two(name){ // named function with parameter
+(function two(name) { // named function with parameter
     console.log(`${name}, DB Connected`);
 })('behan');
 
-(() =>{ // unnamed function
+(() => { // unnamed function
     console.log(`DB Connected`);
 })();
 
-((name) =>{ // unnamed function with parameter
+((name) => { // unnamed function with parameter
     console.log(`${name}, DB Connected`);
 })('sitesh');
+
+
+
+let singleTon = (function () {
+    let instance = null;
+
+    function createInstance() {
+        return {
+            name: "behan",
+            age: 24,
+            score: Math.floor(Math.random() * 100)
+        }
+    }
+
+    return {
+
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+                return instance;
+            }
+            else {
+                return instance;
+            }
+        }
+    }
+})();
+
+let instance1 = singleTon.getInstance();
+let instance2 = singleTon.getInstance();
+
+setTimeout(() => {
+    console.table([instance1, instance2]);
+}, 5000);
+
+for (let i = 0; i <= 5; i++) {
+    (function (para) {
+        setTimeout(() => {
+            console.log(para);
+        }, 3000)
+    })(i);
+
+}
